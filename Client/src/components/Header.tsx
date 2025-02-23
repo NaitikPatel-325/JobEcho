9a3e5bd396b4638510d3de3c71271066d6129989
Het Prajapati <prajapatihet52@gmail.com>
2025-02-23 18:11:41 +0530

    Frontend imporved

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -84,7 +84,7 @@ export default function Header() {
 
         {/* Navigation Links */}
         <nav className="md:flex md:items-center md:w-auto w-full hidden md:block">
-          <ul className="md:flex items-center justify-center text-base text-gray-600">
+          <ul  className="md:flex items-center justify-center text-base text-gray-600">
             <li>
               <a
                 className="relative md:p-4 py-3 px-0 block transform transition-all duration-300 hover:scale-110 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full"
@@ -94,42 +94,6 @@ export default function Header() {
               </a>
             </li>
 
-            <li>
-              <a
-                className="relative md:p-4 py-3 px-0 block transform transition-all duration-300 hover:scale-110 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full"
-                href="/user-experience-form-1"
-              >
-                Share Experience
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="relative md:p-4 py-3 px-0 block transform transition-all duration-300 hover:scale-110 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full"
-                href="/CollegeCompanies"
-              >
-                Interview Experiences
-              </a>
-            </li>
-          </ul>
-
-          <ul className="md:flex items-center justify-center text-base text-gray-400">
-            <li>
-              <a
-                href="/home"
-                className="relative md:p-4 py-3 px-0 block hover:text-white transition-all duration-300"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/features"
-                className="relative md:p-4 py-3 px-0 block hover:text-white transition-all duration-300"
-              >
-                Features
-              </a>
-            </li>
             {isLoggedIn && (
               <li>
                 <a
@@ -140,10 +104,11 @@ export default function Header() {
                 </a>
               </li>
             )}
+
             <li>
               <a
-                href="/InterviewExperience-1"
-                className="relative md:p-4 py-3 px-0 block hover:text-white transition-all duration-300"
+                className="relative md:p-4 py-3 px-0 block transform transition-all duration-300 hover:scale-110 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 hover:after:w-full"
+                href="/CollegeCompanies"
               >
                 Interview Experiences
               </a>
