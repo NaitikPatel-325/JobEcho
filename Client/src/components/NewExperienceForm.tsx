4b5a56f974c2c5a23bc3c14b05fc21ba7658cf51
Apeksha-Shah <apekshashah029@gmail.com>
2025-02-23 14:10:01 +0530

    issue resolved

--- Client/src/components/NewExperienceForm.tsx
+++ Client/src/components/NewExperienceForm.tsx
@@ -11,7 +11,6 @@ import {
 } from "./ui/select";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronRight, ChevronLeft, Plus, X } from "lucide-react";
-import './componentStyles/NewExperienceForm.css'
 
 const formSteps = ["Personal", "Experience", "Results"] as const;
 
