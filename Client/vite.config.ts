07f0b28b626200b07daacbf496a72d9280e0a1ac
Naitik Patel <np03022005@gmail.com>
2025-02-26 14:17:10 +0530

    deployment

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    fs: {
+      strict: false, 
+    },
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    open: false,
+  },
+});
